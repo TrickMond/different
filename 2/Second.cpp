12e1e803345a8fa856cbfd26e27664727177c5b9
leonidXXII <43600469+leonidXXII@users.noreply.github.com>
2018-11-28 01:11:11 +0300

    Вариант 7

--- 2/Second.cpp
+++ 2/Second.cpp
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
 using namespace std;
-
 int main()
 {
     int a, c;
     scanf ("%d%d", &a, &c);
     do // Цикл с постусловием
     {
+
         a++;
         c++;
     } while (a < 0);
